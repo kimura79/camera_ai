fa7b1e6feaefe720fa99826bde3bbe25537a8b4f
kimura79 <ema.saracino@gmail.com>
2025-08-08 21:33:58 +0200

    Update index.dart

--- lib/index.dart
+++ lib/index.dart
@@ -1,3 +1,11 @@
-// Export pages
+// lib/index.dart
+// ===== EXPORT PAGINE PRINCIPALI =====
+
+// Home page
 export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
+
+// Pagina anteprima immagine
 export '/bs_image/bs_image_widget.dart' show BsImageWidget;
+
+// Se hai altre pagine, aggiungile qui
+// export '/pages/altro/altro_widget.dart' show AltroWidget;
