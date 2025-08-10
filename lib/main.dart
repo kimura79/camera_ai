fb3813c16c6de5b0ed43e07920db42d8ad7c13fd
kimura79 <ema.saracino@gmail.com>
2025-08-11 01:41:01 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'pages/home_page/home_page_widget.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      body: ColoredBox(color: Colors.red, child: Center(child: Text('A - UI OK', style: TextStyle(color: Colors.white, fontSize: 24)))),
-    ),
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Test Fotocamera',
+      theme: ThemeData.dark(),
+      home: const HomePageWidget(),
+    );
+  }
 }
