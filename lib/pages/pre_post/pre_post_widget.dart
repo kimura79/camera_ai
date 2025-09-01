c7315f72414eff7e369894f8c1273c17c43ae46a
kimura79 <ema.saracino@gmail.com>
2025-09-01 10:24:15 +0200

    Create pre_post_widget.dart

---
+++ lib/pages/pre_post/pre_post_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PrePostWidget extends StatelessWidget {
+  const PrePostWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Pre/Post")),
+      body: const Center(
+        child: Text("Pagina Pre/Post in costruzione"),
+      ),
+    );
+  }
+}
