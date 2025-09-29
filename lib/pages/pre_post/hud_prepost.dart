63e000a079bd6abc3d27d5d8ea39084942bff481
kimura79 <ema.saracino@gmail.com>
2025-09-29 09:39:46 +0200

    Update hud_prepost.dart

--- lib/pages/pre_post/hud_prepost.dart
+++ lib/pages/pre_post/hud_prepost.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
 import 'package:image/image.dart' as img;
+import 'package:flutter/foundation.dart'; // ✅ Fix per WriteBuffer
 
 class HudPrePostPage extends StatefulWidget {
   final CameraDescription camera;
