21af56a6fce15a19bf18aeef1ebb05d89bc7dd21
kimura79 <ema.saracino@gmail.com>
2025-10-02 08:47:37 +0200

    Update pre_post_widget.dart

--- lib/pages/pre_post/pre_post_widget.dart
+++ lib/pages/pre_post/pre_post_widget.dart
@@ -10,6 +10,7 @@ import 'package:photo_manager/photo_manager.dart';
 import 'package:http/http.dart' as http;
 import 'package:image/image.dart' as img;
 import 'package:path/path.dart' as path;
+import 'package:flutter/rendering.dart';
 
 // importa AnalysisPreview per analisi sul server
 import '../analysis_preview.dart';
