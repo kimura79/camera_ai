1814e3434b263eb4fd99b245ef58cdd78f573a33
kimura79 <ema.saracino@gmail.com>
2025-10-21 19:36:43 +0200

    color: Colors.white,

--- lib/main.dart
+++ lib/main.dart
@@ -135,7 +135,7 @@ class UserTypeSelectorPage extends StatelessWidget {
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [Color(0xFFE9F6FF), Color(0xFFCDEBFA)],
+            color: Colors.white,
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
