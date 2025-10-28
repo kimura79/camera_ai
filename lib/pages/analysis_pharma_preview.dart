dfc4a597c64f0694904bb251e0309092a1170b42
kimura79 <ema.saracino@gmail.com>
2025-10-28 09:49:51 +0100

    Update analysis_pharma_preview.dart

--- lib/pages/analysis_pharma_preview.dart
+++ lib/pages/analysis_pharma_preview.dart
@@ -209,7 +209,7 @@ class _AnalysisPharmaPreviewState extends State<AnalysisPharmaPreview> {
                           color: Colors.red, fontWeight: FontWeight.w600),
                     )),
 
-            const SizedBox(height: 30),
+            const SizedBox(height: 10),
 
             SizedBox(
               width: double.infinity,
