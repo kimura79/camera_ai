7bfac1474e237097e96b360830b542e1a97490ac
kimura79 <ema.saracino@gmail.com>
2025-11-05 12:08:21 +0100

    Update analysis_pharma_preview.dart

--- lib/pages/analysis_pharma_preview.dart
+++ lib/pages/analysis_pharma_preview.dart
@@ -27,9 +27,7 @@ class _AnalysisPharmaPreviewState extends State<AnalysisPharmaPreview> {
   bool _serverReady = false;
   bool _showServerStatus = true;
   Timer? _retryTimer;
-  final List<String> _serverUrls = [
-  "https://drum-cet-grad-stake.trycloudflare.com"
-];
+  final List<String> _serverUrls = ["https://picked-cart-contribution-composition.trycloudflare.com"];
   String _activeServer = "";
 
   // 🔹 per la barra di avanzamento
