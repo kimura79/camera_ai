a1d8734e7d3ca07362849db2718c3d84237e34e5
kimura79 <ema.saracino@gmail.com>
2025-11-05 14:04:46 +0100

    Update analysis_pharma.dart

--- lib/pages/analysis_pharma.dart
+++ lib/pages/analysis_pharma.dart
@@ -23,7 +23,7 @@ class _AnalysisPharmaPageState extends State<AnalysisPharmaPage> {
   Map<String, dynamic>? resultData;
   File? overlayFile;
 
-  final String serverUrl = "https://nightlife-solid-simply-interview.trycloudflare.com";
+  final String serverUrl = "https://sporting-router-mixer-than.trycloudflare.com";
 
   @override
   void initState() {
