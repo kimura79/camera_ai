741f458c24dfeb427a888289a9dde505635d9e77
kimura79 <ema.saracino@gmail.com>
2025-11-06 08:03:11 +0100

    Update analysis_pharma_preview.dart

--- lib/pages/analysis_pharma_preview.dart
+++ lib/pages/analysis_pharma_preview.dart
@@ -27,7 +27,7 @@ class _AnalysisPharmaPreviewState extends State<AnalysisPharmaPreview> {
   bool _serverReady = false;
   bool _showServerStatus = true;
   Timer? _retryTimer;
-  final List<String> _serverUrls = ["https://sporting-router-mixer-than.trycloudflare.com"];
+  final List<String> _serverUrls = ["https://surgical-shake-pop-relationship.trycloudflare.com"];
   String _activeServer = "";
 
   // 🔹 per la barra di avanzamento
